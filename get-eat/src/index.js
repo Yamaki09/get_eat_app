26e13a4d1cbee1a185899f0f7435bc14dec16328
Yamaki09 <kojie09yamashita@gmail.com>
2022-11-03 19:59:34 +0900

    fix: issue

--- get-eat/src/index.js
+++ get-eat/src/index.js
@@ -10,7 +10,7 @@ import { createRoot } from "react-dom/client";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 root.render(
-	<StrictMode>
+	<BrowserRouter>
 		<App />
-	</StrictMode>
+	</BrowserRouter>
 );
